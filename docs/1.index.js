ef75de436ca2c60b1195085f194b2669ba5d140b
OpenCaribbean <104473930+OpenCaribbean@users.noreply.github.com>
2021-07-22 18:55:45 +0000

    update HISTORY.md

---
+++ docs/1.index.js
@@ -0,0 +1,12 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([
+  [1],
+  {
+    /***/ "./pkg/index.js":
+      /*!**********************!*\
+  !*** ./pkg/index.js ***!
+  \**********************/
+      /*! exports provided: createRun, createComment, createTableCell, createTableRow, createDocx, createLevel, createInsert, createTable, createNumbering, createDelete, createParagraph, TableAlignmentType, SpecialIndentKind, VMergeType, BreakType, FontPitchType, WidthType, BorderType, AlignmentType, StyleType, Comment, Delete, Docx, Insert, Level, Numbering, Paragraph, Run, Table, TableCell, TableRow, __wbindgen_string_new, __wbindgen_object_drop_ref, __wbindgen_throw, __wbindgen_rethrow */
+      /***/ function(module, __webpack_exports__, __webpack_require__) {
+        "use strict";
+        eval(
+          '__webpack_require__.r(__webpack_exports__);\n/* WEBPACK VAR INJECTION */(function(TextEncoder, TextDecoder) {/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "createRun", function() { return createRun; });\n/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "createComment", function() { return createComment; });\n/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "createTableCell", function() { return createTableCell; });\n/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "createTableRow", function() { return createTableRow; });\n/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "createDocx", function() { return createDocx; });\n/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "createLevel", function() { return createLevel; });\n/* harmony export (binding) */ __webpack_require__.d(__
