5ebe2f9e65539defb559e2da8aa8f9369c87ed44
OpenCaribbean <104473930+OpenCaribbean@users.noreply.github.com>
2021-08-03 18:42:44 +0000

    Fixed scripts

---
+++ docx-core/src/documents/custom_item_rels.rs
@@ -0,0 +1,8 @@
+use serde::Serialize;
+
+use crate::documents::BuildXML;
+use crate::xml_builder::*;
+
+#[derive(Debug, Clone, PartialEq, Serialize, Default)]
+#[serde(rename_all = "camelCase")]
+pub struct CustomI
