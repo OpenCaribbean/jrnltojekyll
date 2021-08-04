1939abed11b19da297c726e351962db97c6f96fd
OpenCaribbean <104473930+OpenCaribbean@users.noreply.github.com>
2021-08-04 12:29:27 +0000

    Fix undefined error

---
+++ docx-core/src/documents/elements/bold_cs.rs
@@ -0,0 +1,42 @@
+
+use serde::{Deserialize, Serialize, Serializer};
+
+use crate::documents::BuildXML;
+use crate::xml_builder::*;
+
+#[derive(Debug, Clone, Deserialize, PartialEq)]
+pub struct BoldCs {
+    val: bool,
+}
+
+impl BoldCs {
+    pub fn new() -> BoldCs {
+        Default::default()
+    }
+    pub fn disable(mut self) -> BoldCs {
+        self.val = false;
+        self
+    }
+}
+
+impl Default for BoldCs {
+    fn default() -> Self {
+        Self { val: true }
+    }
+}
+
+impl Serialize for BoldCs {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        serializer.serialize_bool(self.val)
+    }
+}
+
+impl BuildXML for BoldCs {
+    fn build(&self) -> Vec<u8> {
+        let b = XMLBuilder::new();
+        b.b_cs().build()
+    }
+}
