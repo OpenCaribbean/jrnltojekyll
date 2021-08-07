dde5554048cb5acdc5158525e38379022bdfe20e
OpenCaribbean <104473930+OpenCaribbean@users.noreply.github.com>
2021-08-07 08:47:11 +0000

    pata_at91: fix ata_host_activate() failure handling

---
+++ docx-core/src/documents/elements/delete_text.rs
@@ -0,0 +1,44 @@
+use serde::{Deserialize, Serialize};
+
+use crate::documents::BuildXML;
+use crate::escape::escape;
+use crate::xml_builder::*;
+
+#[derive(Debug, Clone, Deserialize, Serialize, PartialEq)]
+#[serde(rename_all = "camelCase")]
+pub struct DeleteText {
+    text: String,
+    preserve_space: bool,
+}
+
+impl DeleteText {
+    pub fn new(text: impl Into<String>) -> DeleteText {
+        DeleteText {
+            text: escape(&text.into()),
+            preserve_space: true,
+        }
+    }
+
+    pub(crate) fn without_escape(text: impl Into<String>) -> DeleteText {
+        DeleteText {
+            text: text.into(),
+            preserve_space: true,
+        }
+    }
+}
+
+impl BuildXML for DeleteText {
+    fn build(&self) -> Vec<u8> {
+        XMLBuilder::new().delete_text(&self.text, true).build()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+    #[cfg(test)]
+    use pretty_assertions::assert_eq;
+    use std::str;
+
+   
