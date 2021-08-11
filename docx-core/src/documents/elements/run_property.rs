fa4238311d1e46f336c6c5a15c4316bf276b5307
OpenCaribbean <104473930+OpenCaribbean@users.noreply.github.com>
2021-08-11 22:11:03 +0000

    chore(release): 1.1.0 [skip ci]

--- docx-core/src/documents/elements/run_property.rs
+++ docx-core/src/documents/elements/run_property.rs
@@ -65,4 +65,102 @@ impl RunProperty {
     }
 
     pub fn spacing(mut self, spacing: i32) -> RunProperty {
-        self.chara
+        self.character_spacing = Some(CharacterSpacing::new(spacing));
+        self
+    }
+
+    pub fn color(mut self, color: impl Into<String>) -> RunProperty {
+        self.color = Some(Color::new(color));
+        self
+    }
+
+    pub fn highlight(mut self, color: impl Into<String>) -> RunProperty {
+        self.highlight = Some(Highlight::new(color));
+        self
+    }
+
+    pub fn vert_align(mut self, a: VertAlignType) -> Self {
+        self.vert_align = Some(VertAlign::new(a));
+        self
+    }
+
+    pub fn bold(mut self) -> RunProperty {
+        self.bold = Some(Bold::new());
+        self.bold_cs = Some(BoldCs::new());
+        self
+    }
+
+    pub fn disable_bold(mut self) -> RunProperty {
+        self.bold = Some(Bold::new().disable());
+        self.bold_cs = Some(BoldCs::new().disable());
+        self
+    }
+
+    pub fn italic(mut self) -> RunProperty {
+        self.italic = Some(Italic::new());
+        self.italic_cs = Some(ItalicCs::new());
+        self
+    }
+
+    pub fn strike(mut self) -> RunProperty {
+        self.strike = Some(Strike::new());
+        self
+    }
+
+    pub fn disable_italic(mut self) -> RunProperty {
+        self.italic = Some(Italic::new().disable());
+        self.italic_cs = Some(ItalicCs::new().disable());
+        self
+    }
+
+    pub fn underline(mut self, line_type: impl Into<String>) -> RunProperty {
+        self.underline = Some(Underline::new(line_type));
+        self
+    }
+
+    pub fn vanish(mut self) -> RunProperty {
+        self.vanish = Some(Vanish::new());
+        self
+    }
+
+    pub fn spec_vanish(mut self) -> RunProperty {
+        self.spec_vanish = Some(SpecVanish::new());
+        self
+    }
+
+    pub fn fonts(mut self, font: RunFonts) -> RunProperty {
+        self.fonts = Some(font);
+        self
+    }
+
+    pub fn text_border(mut self, b: TextBorder) -> Self {
+        self.text_border = Some(b);
+        self
+    }
+
+    pub fn delete(mut self, d: Delete) -> Self {
+        self.del = Some(d);
+        self
+    }
+
+    pub fn insert(mut self, i: Insert) -> Self {
+        self.ins = Some(i);
+        self
+    }
+}
+
+impl BuildXML for RunProperty {
+    fn build(&self) -> Vec<u8> {
+        let b = XMLBuilder::new();
+        b.open_run_property()
+            .add_optional_child(&self.sz)
+            .add_optional_child(&self.sz_cs)
+            .add_optional_child(&self.color)
+            .add_optional_child(&self.bold)
+            .add_optional_child(&self.bold_cs)
+            .add_optional_child(&self.italic)
+            .add_optional_child(&self.italic_cs)
+            .add_optional_child(&self.strike)
+            .add_optional_child(&self.highlight)
+            .add_optional_child(&self.underline)
+            .add_
