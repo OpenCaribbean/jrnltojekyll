0a21afa6e282f6bdb8f253ba62d3ffd93e0c69c5
OpenCaribbean <104473930+OpenCaribbean@users.noreply.github.com>
2021-08-21 10:33:57 +0000

    Minor text change.

--- docx-core/src/lib.rs
+++ docx-core/src/lib.rs
@@ -1,4 +1,14 @@
 mod documents;
 mod errors;
 mod escape;
-mod r
+mod reader;
+mod types;
+mod xml_builder;
+mod xml_json;
+mod zipper;
+
+pub use documents::*;
+pub use errors::*;
+pub use reader::*;
+pub use types::*;
+pub use xml_json::*;
