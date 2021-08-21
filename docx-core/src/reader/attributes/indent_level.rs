993a9ec20919e8f12e096b8e08b7d8d3819ea371
OpenCaribbean <104473930+OpenCaribbean@users.noreply.github.com>
2021-08-21 19:15:18 +0000

    adding investment prediction

--- docx-core/src/reader/attributes/indent_level.rs
+++ docx-core/src/reader/attributes/indent_level.rs
@@ -4,4 +4,13 @@ use xml::attribute::OwnedAttribute;
 
 use super::super::errors::*;
 
-pub fn read_indent_level(attrs: &[OwnedAttribute]) -> Result<usize, ReaderErro
+pub fn read_indent_level(attrs: &[OwnedAttribute]) -> Result<usize, ReaderError> {
+    let mut l = 0;
+    for a in attrs {
+        let local_name = &a.name.local_name;
+        if local_name == "ilvl" {
+            l = usize::from_str(&a.value)?;
+        }
+    }
+    Ok(l)
+}
