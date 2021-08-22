1a5c12b2b3c6fa3bb4e2c387b9cfccabad71ea85
OpenCaribbean <104473930+OpenCaribbean@users.noreply.github.com>
2021-08-22 16:42:26 +0000

    pubs

--- docx-core/src/reader/delete.rs
+++ docx-core/src/reader/delete.rs
@@ -20,4 +20,45 @@ impl ElementReader for Delete {
                 }) => {
                     let e = XMLElement::from_str(&name.local_name)
                         .expect("should convert to XMLElement");
-            
+                    match e {
+                        XMLElement::Run => {
+                            del = del.add_run(Run::read(r, attrs)?);
+                        }
+                        XMLElement::CommentRangeStart => {
+                            if let Some(id) = read(&attributes, "id") {
+                                if let Ok(id) = usize::from_str(&id) {
+                                    let comment = Comment::new(id);
+                                    del = del.add_comment_start(comment);
+                                }
+                            }
+                        }
+                        XMLElement::CommentRangeEnd => {
+                            if let Some(id) = read(&attributes, "id") {
+                                if let Ok(id) = usize::from_str(&id) {
+                                    del = del.add_comment_end(id);
+                                }
+                            }
+                        }
+                        _ => {}
+                    }
+                }
+                Ok(XmlEvent::EndElement { name, .. }) => {
+                    let e = XMLElement::from_str(&name.local_name).unwrap();
+                    if e == XMLElement::Delete {
+                        for attr in attrs {
+                            let local_name = &attr.name.local_name;
+                            if local_name == "author" {
+                                del = del.author(&attr.value);
+                            } else if local_name == "date" {
+                                del = del.date(&attr.value);
+                            }
+                        }
+                        return Ok(del);
+                    }
+                }
+                Err(_) => return Err(ReaderError::XMLReadError),
+                _ => {}
+            }
+        }
+    }
+}
