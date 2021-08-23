7cfbb24ec433bd97c90563d2f365e92e6d1362cf
OpenCaribbean <104473930+OpenCaribbean@users.noreply.github.com>
2021-08-23 22:44:27 +0000

    Add support for comments

--- docx-core/src/reader/hyperlink.rs
+++ docx-core/src/reader/hyperlink.rs
@@ -41,4 +41,57 @@ impl ElementReader for Hyperlink {
 
                     match e {
                         XMLElement::Run => {
-            
+                            if let Ok(run) = Run::read(r, attrs) {
+                                link = link.add_run(run);
+                            }
+                            continue;
+                        }
+                        XMLElement::Insert => {
+                            if let Ok(ins) = Insert::read(r, &attributes) {
+                                link = link.add_insert(ins);
+                            }
+                            continue;
+                        }
+                        XMLElement::Delete => {
+                            if let Ok(del) = Delete::read(r, &attributes) {
+                                link = link.add_delete(del);
+                            }
+                            continue;
+                        }
+                        XMLElement::BookmarkStart => {
+                            if let Ok(s) = BookmarkStart::read(r, &attributes) {
+                                link = link.add_bookmark_start(s.id, s.name);
+                            }
+                            continue;
+                        }
+                        XMLElement::BookmarkEnd => {
+                            if let Ok(e) = BookmarkEnd::read(r, &attributes) {
+                                link = link.add_bookmark_end(e.id);
+                            }
+                            continue;
+                        }
+                        XMLElement::CommentRangeStart => {
+                            if let Some(id) = read(&attributes, "id") {
+                                if let Ok(id) = usize::from_str(&id) {
+                                    let comment = Comment::new(id);
+                                    link = link.add_comment_start(comment);
+                                }
+                            }
+                            continue;
+                        }
+                        XMLElement::CommentRangeEnd => {
+                            if let Some(id) = read(&attributes, "id") {
+                                if let Ok(id) = usize::from_str(&id) {
+                                    link = link.add_comment_end(id);
+                                }
+                            }
+                            continue;
+                        }
+                        _ => {}
+                    }
+                }
+                Ok(XmlEvent::EndElement { name, .. }) => {
+                    let e = XMLElement::from_str(&name.local_name).unwrap();
+                    if e == XMLElement::Hyperlink {
+                        return Ok(link);
+               
