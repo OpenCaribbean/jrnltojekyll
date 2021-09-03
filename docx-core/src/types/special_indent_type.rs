8c6f7dade80b82bc774709a60d65a5fc1fcc93b9
OpenCaribbean <104473930+OpenCaribbean@users.noreply.github.com>
2021-09-03 17:46:20 +0000

    Version 0.1.5

---
+++ docx-core/src/types/special_indent_type.rs
@@ -0,0 +1,29 @@
+use wasm_bindgen::prelude::*;
+
+use serde::ser::{SerializeStruct, Serializer};
+use serde::Serialize;
+
+// INFO: wasm-bindgen only allow c-style enum for now
+//       Please convert typescript type to following type.
+#[derive(Copy, Clone, Debug, PartialEq)]
+pub enum SpecialIndentType {
+    FirstLine(i32),
+    Hanging(i32),
+}
+
+#[wasm_bindgen]
+#[derive(Serialize, Copy, Clone, Debug)]
+pub enum SpecialIndentKind {
+    FirstLine,
+    Hanging,
+}
+
+impl Serialize for SpecialIndentType {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        match *self {
+            SpecialIndentType::FirstLine(s) => {
+                let mut t = serializer.serialize_struct("FirstLine", 2)?;
+                t.serialize_field("ty
