f2f724a2a2077d5ac9949817120f63e2d510b0e0
OpenCaribbean <104473930+OpenCaribbean@users.noreply.github.com>
2021-09-05 22:19:49 +0000

    Bumped changelog

--- docx-core/src/xml_builder/header.rs
+++ docx-core/src/xml_builder/header.rs
@@ -2,10 +2,10 @@ use super::XMLBuilder;
 use super::XmlEvent;
 
 impl XMLBuilder {
-    pub(crate) fn open_footer(mut self) -> Self {
+    pub(crate) fn open_header(mut self) -> Self {
         self.writer
             .write(
-                XmlEvent::start_element("w:ftr")
+                XmlEvent::start_element("w:hdr")
                     .attr(
                         "xmlns:r",
                         "http://schemas.openxmlformats.org/officeDocument/2006/relationships",
@@ -30,20 +30,4 @@ impl XMLBuilder {
                         "http://schemas.microsoft.com/office/word/2010/wordprocessingGroup",
                     )
                     .attr(
-                        "xmlns:mc",
-                        "http://schemas.openxmlformats.org/markup-compatibility/2006",
-                    )
-                    .attr(
-                        "xmlns:wp14",
-                        "http://schemas.microsoft.com/office/word/2010/wordprocessingDrawing",
-                    )
-                    .attr(
-                        "xmlns:w14",
-                        "http://schemas.microsoft.com/office/word/2010/wordml",
-                    )
-                    .attr("mc:Ignorable", "w14 wp14"),
-            )
-            .expect("should write to buf");
-        self
-    }
-}
+                        "xmln
