ecb7801868ccfb2c59f9e6df73fab93ecf3aa7b7
OpenCaribbean <104473930+OpenCaribbean@users.noreply.github.com>
2021-09-06 13:57:33 +0000

    bugs fixed

--- docx-core/src/xml_builder/settings.rs
+++ docx-core/src/xml_builder/settings.rs
@@ -5,4 +5,21 @@ impl XMLBuilder {
     pub(crate) fn open_settings(mut self) -> Self {
         self.writer
             .write(
-                XmlEvent::start_element(
+                XmlEvent::start_element("w:settings")
+                    .attr(
+                        "xmlns:w",
+                        "http://schemas.openxmlformats.org/wordprocessingml/2006/main",
+                    )
+                    .attr(
+                        "xmlns:w14",
+                        "http://schemas.microsoft.com/office/word/2010/wordml",
+                    )
+                    .attr(
+                        "xmlns:w15",
+                        "http://schemas.microsoft.com/office/word/2012/wordml",
+                    ),
+            )
+            .expect("should write to buf");
+        self
+    }
+}
